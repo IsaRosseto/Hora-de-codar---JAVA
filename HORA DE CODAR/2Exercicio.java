fd75ec442955d3ad738a2685bd9256f5e826c561
Isabella Rosseto <92994715+IsaRosseto@users.noreply.github.com>
2022-11-23 23:35:24 -0300

    Update and rename Exercicio2.java to 2Exercicio.java

--- HORA DE CODAR/2Exercicio.java
+++ HORA DE CODAR/2Exercicio.java
@@ -2,7 +2,7 @@
 Escreva um programa em Java em que o usuário informe o seu nome e exiba a mensagem "Olá, [NomeDoUsuario]". */
 
 import java.util.Scanner;
-public class Exercicio2 {
+public class 2Exercicio {
     public static void main(String[]args)
     {
         System.out.println("Qual é o seu nome ?");
