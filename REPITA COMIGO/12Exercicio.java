146b96ef039fe1e8fd0a5b8c89c5ed767b9d87a8
Isabella Rosseto <92994715+IsaRosseto@users.noreply.github.com>
2022-11-23 23:53:30 -0300

    Update and rename Exercicio12.java to 12Exercicio.java

--- REPITA COMIGO/12Exercicio.java
+++ REPITA COMIGO/12Exercicio.java
@@ -1,7 +1,7 @@
 /* 12 - 10, 9, 8, 7, 6, 5, 4, 3, 2, 1...
 Escreva um algoritmo para imprimir os números de 1 (inclusive) a 10 (inclusive) em ordem decrescente. */
 
-public class Exercicio12 {
+public class 12Exercicio {
     public static void main(String[] args) {
 
 int contagem = 10
